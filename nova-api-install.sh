43a08c685fbcdfd65854e15ebc65abac6eb39f0e
stack <stack@stack.(none)>
2012-06-21 15:03:43 +0400

    moved from common

--- nova-api-install.sh
+++ nova-api-install.sh
@@ -7,4 +7,7 @@ check_root
 apt-get install -y nova-api nova-cert nova-consoleauth nova-scheduler nova-network
 
 service nova-network stop
+nova-manage db sync
+nova-manage network create private --fixed_range_v4=$FIXED_RANGE --num_networks=1 --bridge=br$FIRST_VLAN --bridge_interface=$VLAN_IFACE
+nova-manage floating create --ip_range=$FLOATING_RANGE --interface=$PUBLIC_IFACE
 

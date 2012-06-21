05baeb3f296171f72ea93ff810153ccd7d25e745
stack <stack@stack.(none)>
2012-06-21 15:08:21 +0400

    added api flags

--- nova-api-install.sh
+++ nova-api-install.sh
@@ -4,10 +4,16 @@
 
 check_root
 
+NOVA_CONFIG=/etc/nova/nova.conf
+
 apt-get install -y nova-api nova-cert nova-consoleauth nova-scheduler nova-network
 
 service nova-network stop
+
 nova-manage db sync
 nova-manage network create private --fixed_range_v4=$FIXED_RANGE --num_networks=1 --bridge=br$FIRST_VLAN --bridge_interface=$VLAN_IFACE
 nova-manage floating create --ip_range=$FLOATING_RANGE --interface=$PUBLIC_IFACE
 
+echo "--connection_type=libvirt" >> $NOVA_CONFIG
+echo "--public_interface=$PUBLIC_INTERFACE" >> $NOVA_CONFIG
+echo "--multi_host" >> $NOVA_CONFIG
